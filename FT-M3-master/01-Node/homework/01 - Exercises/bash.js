e857254d34b1e38149a3504349b1d050ac3e7515
RonalReds <ronalrojas.92@gmail.com>
2023-11-27 16:32:05 -0400

    HomeWork NODE

---
+++ FT-M3-master/01-Node/homework/01 - Exercises/bash.js
@@ -0,0 +1,31 @@
+const process = require('process');
+const { Z_ASCII } = require('zlib');
+const commands = require('./commands/index.js');
+
+function bash() { 
+   process.stdout.write("prompt > ")
+   process.stdin.on("data", (data) => {
+      let args = data.toString().trim().split(' ');
+      let cmd = args.shift();
+      if (commands.hasOwnProperty(cmd)) {
+         commands[cmd](print, args.join(' '));
+      } else {
+         print(`command not found: ${cmd}`)
+      }
+    })
+   
+}
+
+
+const print = (output) => {
+   process.stdout.write(output);
+   process.stdout.write("\nprompt > ");
+   
+
+}
+
+bash();
+module.exports = {
+   print,
+   bash,
+};
